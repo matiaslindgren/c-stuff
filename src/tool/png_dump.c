621a5ec757bb7eeff987a8ef71752fe7456db90d
Matias Lindgren <matias.lindgren@iki.fi>
2023-01-14 23:51:03 +0100

    fix png tools

--- src/tool/png_dump.c
+++ src/tool/png_dump.c
@@ -1,40 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "stufflib_argv.h"
 #include "stufflib_inflate.h"
 #include "stufflib_png.h"
 
 int main(int argc, char* const argv[argc + 1]) {
   if (!(argc == 2 || argc == 3)) {
-    fprintf(stderr, "usage: %s png_path [data_dump_count]\n", argv[0]);
+    fprintf(stderr, "usage: %s png_path [-v]\n", argv[0]);
     return EXIT_FAILURE;
   }
   const char* png_path = argv[1];
-  const size_t data_dump_count = argc == 3 ? strtoull(argv[2], 0, 10) : 0;
-
-  stufflib_png_image img = stufflib_png_read_image(png_path);
-  if (!img.size) {
-    return EXIT_FAILURE;
-  }
+  const int verbose = stufflib_argv_parse_flag(argc, argv, "-v");
 
   printf("FILE\n  %s\n", png_path);
-  printf("HEADER\n");
-  stufflib_png_dump_img_meta(stdout, img);
 
-  if (data_dump_count) {
-    printf("DATA\n");
-    printf("data size %zu\n", img.size);
-    for (size_t i = 0; i < img.size; ++i) {
-      if (i < data_dump_count) {
-        printf("%3zu: %02x\n", i, img.data[i]);
-      } else {
-        printf("... data_dump_count %zu reached\n", data_dump_count);
-        break;
-      }
+  {
+    printf("CHUNKS\n");
+    stufflib_png_chunks chunks = stufflib_png_read_chunks(png_path);
+    if (!chunks.count) {
+      return EXIT_FAILURE;
     }
+    for (size_t i = 0; i < chunks.count; ++i) {
+      stufflib_png_chunk chunk = chunks.chunks[i];
+      printf("%3zu (%5zu bytes): %s\n",
+             i,
+             chunk.data.size,
+             stufflib_png_chunk_types[chunk.type]);
+    }
+    stufflib_png_chunks_destroy(chunks);
   }
 
-  stufflib_png_destroy(&img);
+  {
+    stufflib_png_image img = stufflib_png_read_image(png_path);
+    if (!img.data.size) {
+      return EXIT_FAILURE;
+    }
+    printf("FILE\n  %s\n", png_path);
+    printf("HEADER\n");
+    stufflib_png_dump_img_meta(stdout, img);
+    printf("DATA (%zu bytes)\n", img.data.size);
+    if (verbose) {
+      stufflib_png_dump_img_data(stdout, img);
+    }
+    stufflib_png_image_destroy(img);
+  }
 
   return EXIT_SUCCESS;
 }
--- src/tool/png_idat.c
+++ src/tool/png_idat.c
@@ -3,18 +3,56 @@
 
 #include "stufflib_png.h"
 
+int dump_chunk(stufflib_png_chunks chunks, const size_t index) {
+  if (index >= chunks.count) {
+    fprintf(stderr,
+            "chunk index %zu should be less than chunk count %zu\n",
+            index,
+            chunks.count);
+    return 0;
+  }
+  stufflib_png_chunk chunk = chunks.chunks[index];
+  if (chunk.type != stufflib_png_IDAT) {
+    fprintf(stderr,
+            "chunk index %zu points to chunk of type %s, not IDAT\n",
+            index,
+            stufflib_png_chunk_types[chunk.type]);
+    return 0;
+  }
+  fwrite(chunk.data.data, sizeof(chunk.data.data[0]), chunk.data.size, stdout);
+  return 1;
+}
+
 int main(int argc, char* const argv[argc + 1]) {
-  if (argc != 2) {
-    fprintf(stderr, "usage: %s png_path\n", argv[0]);
+  if (!(argc == 2 || argc == 3)) {
+    fprintf(stderr, "usage: %s png_path [chunk_index]\n", argv[0]);
     return EXIT_FAILURE;
   }
   const char* png_path = argv[1];
-  struct stufflib_png_image img = stufflib_png_read(png_path);
-  if (!img.size) {
+  stufflib_png_chunks chunks = stufflib_png_read_chunks(png_path);
+  if (!chunks.count) {
     return EXIT_FAILURE;
   }
-  fwrite(img.data, sizeof(img.data[0]), img.size, stdout);
-  fflush(stdout);
-  free(img.data);
-  return EXIT_SUCCESS;
+
+  int status = EXIT_SUCCESS;
+
+  if (argc == 3) {
+    const size_t chunk_index = strtoull(argv[2], 0, 10);
+    if (!dump_chunk(chunks, chunk_index)) {
+      status = EXIT_FAILURE;
+    }
+  } else {
+    for (size_t i = 0; i < chunks.count; ++i) {
+      stufflib_png_chunk chunk = chunks.chunks[i];
+      if (chunk.type == stufflib_png_IDAT) {
+        if (!dump_chunk(chunks, i)) {
+          status = EXIT_FAILURE;
+          break;
+        }
+      }
+    }
+  }
+
+  stufflib_png_chunks_destroy(chunks);
+  return status;
 }
