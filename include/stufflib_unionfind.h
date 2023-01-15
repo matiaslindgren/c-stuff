6fa8fd7e79a484513379e72cd3a37bd9ec2f6276
Matias Lindgren <matias.lindgren@iki.fi>
2023-01-15 14:31:07 +0100

    cleanup union find api

--- include/stufflib_unionfind.h
+++ include/stufflib_unionfind.h
@@ -5,22 +5,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-size_t* stufflib_unionfind_init(const size_t n) {
-  size_t* parents = calloc(n, sizeof(size_t));
+typedef struct stufflib_unionfind stufflib_unionfind;
+struct stufflib_unionfind {
+  size_t size;
+  size_t* parents;
+};
+
+stufflib_unionfind stufflib_unionfind_new(const size_t num_sets) {
+  size_t* parents = calloc(num_sets, sizeof(size_t));
   if (!parents) {
-    return 0;
+    return (stufflib_unionfind){0};
   }
-  for (size_t i = 0; i < n; ++i) {
+  for (size_t i = 0; i < num_sets; ++i) {
     parents[i] = SIZE_MAX;
   }
-  return parents;
+  return (stufflib_unionfind){
+      .size = num_sets,
+      .parents = parents,
+  };
 }
 
-size_t stufflib_unionfind_find_root(const size_t n,
-                                    size_t parents[n],
-                                    size_t index) {
-  while (parents[index] != SIZE_MAX) {
-    index = parents[index];
+void stufflib_unionfind_destroy(stufflib_unionfind uf[static 1]) {
+  free(uf->parents);
+  *uf = (stufflib_unionfind){0};
+}
+
+size_t stufflib_unionfind_find_root(const stufflib_unionfind uf, size_t index) {
+  while (uf.parents[index] != SIZE_MAX) {
+    index = uf.parents[index];
   }
   return index;
 }
@@ -32,32 +44,29 @@ size_t _stufflib_unionfind_exchange_root(size_t parent[static 1],
   return old_root;
 }
 
-void _stufflib_unionfind_find_replace(const size_t n,
-                                      size_t parents[const n],
+void _stufflib_unionfind_find_replace(const stufflib_unionfind uf,
                                       size_t index,
                                       const size_t new_root) {
-  while (parents[index] != SIZE_MAX) {
-    index = _stufflib_unionfind_exchange_root(&parents[index], new_root);
+  while (uf.parents[index] != SIZE_MAX) {
+    index = _stufflib_unionfind_exchange_root(uf.parents + index, new_root);
   }
-  parents[index] = new_root;
+  uf.parents[index] = new_root;
 }
 
-size_t _stufflib_unionfind_find_compress(const size_t n,
-                                         size_t parents[const n],
+size_t _stufflib_unionfind_find_compress(const stufflib_unionfind uf,
                                          size_t index) {
-  const size_t root = stufflib_unionfind_find_root(n, parents, index);
+  const size_t root = stufflib_unionfind_find_root(uf, index);
   while (index != root) {
-    index = _stufflib_unionfind_exchange_root(&parents[index], root);
+    index = _stufflib_unionfind_exchange_root(uf.parents + index, root);
   }
   return root;
 }
 
-size_t stufflib_unionfind_union(const size_t n,
-                                size_t parents[const n],
+size_t stufflib_unionfind_union(const stufflib_unionfind uf,
                                 const size_t lhs,
                                 const size_t rhs) {
-  size_t root = _stufflib_unionfind_find_compress(n, parents, lhs);
-  _stufflib_unionfind_find_replace(n, parents, rhs, root);
+  size_t root = _stufflib_unionfind_find_compress(uf, lhs);
+  _stufflib_unionfind_find_replace(uf, rhs, root);
   return root;
 }
 #endif  // _STUFFLIB_UNIONFIND_H_INCLUDED
