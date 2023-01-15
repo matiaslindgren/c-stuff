a45b4bb4536582f4094981ff52300e5ec96e7e79
Matias Lindgren <matias.lindgren@iki.fi>
2023-01-15 14:09:52 +0100

    update png tests

--- src/test/test_stufflib_png.c
+++ src/test/test_stufflib_png.c
@@ -6,7 +6,7 @@
 #include "stufflib_misc.h"
 #include "stufflib_png.h"
 
-int _test_read_single_pixel_chunks(const int verbose) {
+int test_read_single_pixel_chunks(const int verbose) {
   for (size_t i = 0; i < 3; ++i) {
     const char* png_path = (const char*[]){
         "./test-data/ff0000-1x1-rgb-nocomp.png",
@@ -29,7 +29,7 @@ int _test_read_single_pixel_chunks(const int verbose) {
   return 0;
 }
 
-int _test_read_large_image_with_many_chunks(const int verbose) {
+int test_read_large_image_with_many_chunks(const int verbose) {
   const char* png_path = "./test-data/asan.png";
   if (verbose) {
     printf("%s\n", png_path);
@@ -51,7 +51,7 @@ int _test_read_large_image_with_many_chunks(const int verbose) {
   return 0;
 }
 
-int _test_read_single_pixel_header(const int verbose) {
+int test_read_single_pixel_header(const int verbose) {
   for (size_t i = 0; i < 3; ++i) {
     const char* png_path = (const char*[]){
         "./test-data/ff0000-1x1-rgb-nocomp.png",
@@ -76,7 +76,7 @@ int _test_read_single_pixel_header(const int verbose) {
   return 0;
 }
 
-int _test_read_img_header(const int verbose) {
+int test_read_img_header(const int verbose) {
   {
     const char* png_path = "./test-data/white-square-rgba-dynamic.png";
     if (verbose) {
@@ -152,9 +152,9 @@ int _test_read_img_header(const int verbose) {
   return 0;
 }
 
-static inline int _test_read_single_pixel(const char* png_path,
-                                          const size_t on_index,
-                                          const int verbose) {
+static inline int test_read_single_pixel(const char* png_path,
+                                         const size_t on_index,
+                                         const int verbose) {
   if (verbose) {
     printf("%s\n", png_path);
   }
@@ -171,48 +171,79 @@ static inline int _test_read_single_pixel(const char* png_path,
   assert(pixel.header.bit_depth == 8);
   assert(pixel.header.color_type == stufflib_png_rgb);
 
-  assert(pixel.data.size == 4);
-  unsigned char expected_data[4] = {0};
-  expected_data[on_index] = 255;
-  for (size_t c = 0; c < 4; ++c) {
-    assert(pixel.data.data[c] == expected_data[c]);
+  assert(pixel.data.size == 9 * 3);
+  unsigned char expected_data[9 * 3] = {0};
+  expected_data[4 * 3 + on_index] = 255;
+  for (size_t i = 0; i < STUFFLIB_ARRAY_LEN(expected_data); ++i) {
+    assert(pixel.data.data[i] == expected_data[i]);
   }
 
   stufflib_png_image_destroy(pixel);
   return 0;
 }
 
-int _test_read_single_pixel_no_compression(const int verbose) {
-  for (size_t i = 0; i < 3; ++i) {
+int test_read_single_pixel_no_compression(const int verbose) {
+  for (size_t on_pixel = 0; on_pixel < 3; ++on_pixel) {
     const char* png_path = (const char*[]){
         "./test-data/ff0000-1x1-rgb-nocomp.png",
         "./test-data/00ff00-1x1-rgb-nocomp.png",
         "./test-data/0000ff-1x1-rgb-nocomp.png",
-    }[i];
-    const size_t on_pixel = i + 1;
-    if (_test_read_single_pixel(png_path, on_pixel, verbose)) {
+    }[on_pixel];
+    if (test_read_single_pixel(png_path, on_pixel, verbose)) {
       return 1;
     }
   }
   return 0;
 }
 
-int _test_read_single_pixel_with_fixed_compression(const int verbose) {
-  for (size_t i = 0; i < 3; ++i) {
+int test_read_single_pixel_with_fixed_compression(const int verbose) {
+  for (size_t on_pixel = 0; on_pixel < 3; ++on_pixel) {
     const char* png_path = (const char*[]){
         "./test-data/ff0000-1x1-rgb-fixed.png",
         "./test-data/00ff00-1x1-rgb-fixed.png",
         "./test-data/0000ff-1x1-rgb-fixed.png",
-    }[i];
-    const size_t on_pixel = i + 1;
-    if (_test_read_single_pixel(png_path, on_pixel, verbose)) {
+    }[on_pixel];
+    if (test_read_single_pixel(png_path, on_pixel, verbose)) {
       return 1;
     }
   }
   return 0;
 }
 
-int _test_read_small_images_with_dynamic_compression(const int verbose) {
+int test_read_rgba_image_with_dynamic_compression(const int verbose) {
+  const char* png_path = "./test-data/white-square-rgba-dynamic.png";
+  if (verbose) {
+    printf("%s\n", png_path);
+  }
+  stufflib_png_image white_square = stufflib_png_read_image(png_path);
+  if (!white_square.data.size) {
+    return 1;
+  }
+  if (verbose) {
+    stufflib_png_dump_img_meta(stdout, white_square);
+  }
+  const size_t width = 26;
+  const size_t height = 28;
+  const size_t bpp = 4;
+  assert(white_square.header.width == width);
+  assert(white_square.header.height == height);
+  assert(white_square.header.bit_depth == 8);
+  assert(white_square.header.color_type == stufflib_png_rgba);
+  assert(white_square.data.size == bpp * (width + 2) * (height + 2));
+  for (size_t y = 1; y < height + 1; ++y) {
+    for (size_t x = 1; x < width + 1; ++x) {
+      const unsigned char* px =
+          white_square.data.data + bpp * (y * (width + 2) + x);
+      for (size_t byte = 0; byte < bpp; ++byte) {
+        assert(px[byte] == 0xff);
+      }
+    }
+  }
+  stufflib_png_image_destroy(white_square);
+  return 0;
+}
+
+int test_read_small_images_with_dynamic_compression(const int verbose) {
   for (size_t i = 0; i < 3; ++i) {
     const char* png_path = (const char*[]){
         "./test-data/0099ee-80x160-rgb-dynamic.png",
@@ -241,18 +272,13 @@ int _test_read_small_images_with_dynamic_compression(const int verbose) {
     assert(img.header.height == height);
     assert(img.header.bit_depth == 8);
     assert(img.header.color_type == stufflib_png_rgb);
-
-    assert(img.data.size == height + width * height * 3);
-    for (size_t y = 0; y < height; ++y) {
-      for (size_t x = 0; x < width; ++x) {
-        const size_t i = y + 1 + 3 * (y * width + x);
-        const unsigned r = img.data.data[i + 0];
-        const unsigned g = img.data.data[i + 1];
-        const unsigned b = img.data.data[i + 2];
-        printf("y%zu x%zu i%zu r%u g%u b%u %zu\n", y, x, i, r, g, b, rgb);
-        assert(r == ((rgb & 0xff0000) >> 16));
-        assert(g == ((rgb & 0x00ff00) >> 8));
-        assert(b == ((rgb & 0x0000ff) >> 0));
+    assert(img.data.size == 3 * (width + 2) * (height + 2));
+    for (size_t y = 1; y < height + 1; ++y) {
+      for (size_t x = 1; x < width + 1; ++x) {
+        const unsigned char* px = img.data.data + 3 * (y * (width + 2) + x);
+        assert(px[0] == ((rgb & 0xff0000) >> 16));
+        assert(px[1] == ((rgb & 0x00ff00) >> 8));
+        assert(px[2] == ((rgb & 0x0000ff) >> 0));
       }
     }
     stufflib_png_image_destroy(img);
@@ -260,7 +286,7 @@ int _test_read_small_images_with_dynamic_compression(const int verbose) {
   return 0;
 }
 
-int _test_read_large_images_with_dynamic_compression(const int verbose) {
+int test_read_large_images_with_dynamic_compression(const int verbose) {
   {
     const char* png_path = "./test-data/aabbcc-1600x1600-rgb-dynamic.png";
     if (verbose) {
@@ -279,17 +305,18 @@ int _test_read_large_images_with_dynamic_compression(const int verbose) {
     assert(square.header.height == size);
     assert(square.header.bit_depth == 8);
     assert(square.header.color_type == stufflib_png_rgb);
-    assert(square.data.size == size + size * size * 3);
-    for (size_t y = 0; y < size; ++y) {
-      for (size_t x = 0; x < size; ++x) {
-        const size_t i = y + 1 + y * size * 3 + x * 3;
-        assert(square.data.data[i + 0] == 0xaa);
-        assert(square.data.data[i + 1] == 0xbb);
-        assert(square.data.data[i + 2] == 0xcc);
+    assert(square.data.size == 3 * (size + 2) * (size + 2));
+    for (size_t y = 1; y < size + 1; ++y) {
+      for (size_t x = 1; x < size + 1; ++x) {
+        const unsigned char* px = square.data.data + 3 * (y * (size + 2) + x);
+        assert(px[0] == 0xaa);
+        assert(px[1] == 0xbb);
+        assert(px[2] == 0xcc);
       }
     }
     stufflib_png_image_destroy(square);
   }
+
   {
     const char* png_path = "./test-data/asan.png";
     if (verbose) {
@@ -303,11 +330,14 @@ int _test_read_large_images_with_dynamic_compression(const int verbose) {
     if (verbose) {
       stufflib_png_dump_img_meta(stdout, asan);
     }
-    assert(asan.header.width == 958);
-    assert(asan.header.height == 458);
+    const size_t width = 958;
+    const size_t height = 458;
+    assert(asan.header.width == width);
+    assert(asan.header.height == height);
     assert(asan.header.bit_depth == 8);
     assert(asan.header.color_type == stufflib_png_rgb);
-    assert(asan.data.size == 24640);
+    assert(asan.filter.size == height);
+    assert(asan.data.size == 3 * (width + 2) * (height + 2));
     stufflib_png_image_destroy(asan);
   }
   return 0;
@@ -319,32 +349,35 @@ int main(int argc, char* const argv[argc + 1]) {
   const int verbose = stufflib_argv_parse_flag(argc, argv, "-v");
 
   test_function* tests[] = {
-      _test_read_single_pixel_chunks,
-      _test_read_large_image_with_many_chunks,
-      _test_read_single_pixel_header,
-      _test_read_img_header,
-      _test_read_single_pixel_no_compression,
-      _test_read_single_pixel_with_fixed_compression,
-      _test_read_small_images_with_dynamic_compression,
-      _test_read_large_images_with_dynamic_compression,
+      test_read_single_pixel_chunks,
+      test_read_large_image_with_many_chunks,
+      test_read_single_pixel_header,
+      test_read_img_header,
+      test_read_single_pixel_no_compression,
+      test_read_single_pixel_with_fixed_compression,
+      test_read_rgba_image_with_dynamic_compression,
+      test_read_small_images_with_dynamic_compression,
+      test_read_large_images_with_dynamic_compression,
   };
   const char* test_names[] = {
-      "read_single_pixel_chunks",
-      "read_large_image_with_many_chunks",
-      "read_single_pixel_header",
-      "read_img_header",
-      "read_single_pixel_no_compression",
-      "read_single_pixel_with_fixed_compression",
-      "read_small_images_with_dynamic_compression",
-      "read_large_images_with_dynamic_compression",
+      "test_read_single_pixel_chunks",
+      "test_read_large_image_with_many_chunks",
+      "test_read_single_pixel_header",
+      "test_read_img_header",
+      "test_read_single_pixel_no_compression",
+      "test_read_single_pixel_with_fixed_compression",
+      "test_read_rgba_image_with_dynamic_compression",
+      "test_read_small_images_with_dynamic_compression",
+      "test_read_large_images_with_dynamic_compression",
   };
 
   for (size_t t = 0; t < STUFFLIB_ARRAY_LEN(tests); ++t) {
     if (verbose) {
-      printf("\ntesting %s\n", test_names[t]);
+      printf("\n%s\n", test_names[t]);
     }
-    if (tests[t](verbose)) {
-      fprintf(stderr, "test %zu failed\n", t + 1);
+    const int failed = tests[t](verbose);
+    if (failed) {
+      fprintf(stderr, "test %s (%zu) failed\n", test_names[t], t);
       return EXIT_FAILURE;
     }
   }
