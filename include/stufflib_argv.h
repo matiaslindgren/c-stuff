260335bf6e56acba4058439e11ab3b4365c29f6a
Matias Lindgren <matias.lindgren@iki.fi>
2023-01-23 22:20:05 +0100

    add uint argv parser

--- include/stufflib_argv.h
+++ include/stufflib_argv.h
@@ -5,13 +5,32 @@
 
 int stufflib_argv_parse_flag(int argc,
                              char* const argv[argc + 1],
-                             const char* flag) {
+                             const char* arg) {
   for (size_t i = 1; i < argc; ++i) {
-    if (strcmp(argv[i], flag) == 0) {
+    if (strcmp(argv[i], arg) == 0) {
       return 1;
     }
   }
   return 0;
+}
+
+size_t stufflib_argv_parse_uint(int argc,
+                                char* const argv[argc + 1],
+                                const char* arg,
+                                const size_t default_) {
+  const size_t arg_len = strlen(arg);
+  if (!arg_len) {
+    return default_;
+  }
+  for (size_t i = 1; i < argc; ++i) {
+    if (strncmp(argv[i], arg, arg_len) == 0) {
+      const char* value = strchr(argv[i], '=');
+      if (value) {
+        return strtoull(value + 1, 0, 10);
+      }
+    }
+  }
+  return default_;
 }
 
 #endif  // _STUFFLIB_ARGV_H_INCLUDED
