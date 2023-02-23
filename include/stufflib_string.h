fbeaada111dfa29f084bde00a6c06090a6d3b218
Matias Lindgren <matias.lindgren@iki.fi>
2023-02-23 23:28:59 +0100

    implement unicode string init from file

--- include/stufflib_string.h
+++ include/stufflib_string.h
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 
 #include "stufflib_data.h"
+#include "stufflib_io.h"
 #include "stufflib_iterator.h"
 #include "stufflib_macros.h"
 #include "stufflib_unicode.h"
@@ -14,10 +15,15 @@ struct stufflib_string {
   stufflib_data utf8_data;
 };
 
+void stufflib_string_delete(stufflib_string string[static 1]) {
+  stufflib_data_delete(&(string->utf8_data));
+  *string = (stufflib_string){0};
+}
+
 stufflib_string stufflib_string_from_utf8(
     const stufflib_data utf8_data[const static 1]) {
   if (!stufflib_unicode_is_valid_utf8(utf8_data)) {
-    STUFFLIB_LOG_ERROR("cannot create string from invalid UTF-8 data");
+    STUFFLIB_LOG_ERROR("UTF-8 decode error, cannot initialize string");
     return (stufflib_string){0};
   }
   return (stufflib_string){
@@ -26,15 +32,31 @@ stufflib_string stufflib_string_from_utf8(
   };
 }
 
+stufflib_string stufflib_string_from_file(const char filename[const static 1]) {
+  stufflib_data utf8_data = stufflib_data_create(0);
+  for (stufflib_iterator file_iter = stufflib_file_iter_open(filename);
+       !file_iter.is_done(&file_iter);
+       file_iter.advance(&file_iter)) {
+    stufflib_data* buffer = file_iter.get_item(&file_iter);
+    stufflib_data_extend(&utf8_data, buffer);
+  }
+  if (!stufflib_unicode_is_valid_utf8(&utf8_data)) {
+    STUFFLIB_LOG_ERROR("cannot decode '%s' as UTF-8", filename);
+    stufflib_data_delete(&utf8_data);
+    return (stufflib_string){0};
+  }
+  return stufflib_string_from_utf8(&utf8_data);
+}
+
 stufflib_string stufflib_string_slice(const stufflib_string str[const static 1],
                                       const size_t begin,
                                       const size_t end) {
   stufflib_iterator begin_iter = stufflib_unicode_iter(&(str->utf8_data));
-  while (begin_iter.pos < begin && !begin_iter.end(&begin_iter)) {
+  while (begin_iter.pos < begin && !begin_iter.is_done(&begin_iter)) {
     begin_iter.advance(&begin_iter);
   }
   stufflib_iterator end_iter = begin_iter;
-  while (end_iter.pos < end && !end_iter.end(&end_iter)) {
+  while (end_iter.pos < end && !end_iter.is_done(&end_iter)) {
     end_iter.advance(&end_iter);
   }
   stufflib_data utf8_slice =
@@ -46,16 +68,16 @@ stufflib_string stufflib_string_strstr(
     const stufflib_string str[const static 1],
     const stufflib_string substr[const static 1]) {
   for (stufflib_iterator str_iter = stufflib_unicode_iter(&(str->utf8_data));
-       !str_iter.end(&str_iter);
+       !str_iter.is_done(&str_iter);
        str_iter.advance(&str_iter)) {
     stufflib_iterator lhs = str_iter;
     stufflib_iterator rhs = stufflib_unicode_iter(&(substr->utf8_data));
     bool match = true;
     while (match) {
-      if (rhs.end(&rhs)) {
+      if (rhs.is_done(&rhs)) {
         break;
       }
-      if (lhs.end(&lhs)) {
+      if (lhs.is_done(&lhs)) {
         match = false;
         break;
       }
@@ -77,7 +99,7 @@ stufflib_string stufflib_string_strstr(
 void stufflib_string_fprint(FILE stream[const static 1],
                             const stufflib_string str[const static 1]) {
   for (stufflib_iterator iter = stufflib_unicode_iter(&(str->utf8_data));
-       !iter.end(&iter);
+       !iter.is_done(&iter);
        iter.advance(&iter)) {
     const wchar_t item = stufflib_unicode_iter_decode_item(&iter);
     if (fwprintf(stream, L"%lc", item) < 0) {
