144e0f7a503c3136663b0130c7d21ce2b120923a
Matias Lindgren <matias.lindgren@iki.fi>
2024-05-31 19:00:16 +0200

    add slurp file utility

---
+++ include/stufflib_filesystem.h
@@ -0,0 +1,21 @@
+#ifndef _STUFFLIB_FILESYSTEM_H_INCLUDED
+#define _STUFFLIB_FILESYSTEM_H_INCLUDED
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "stufflib_io.h"
+#include "stufflib_span.h"
+
+struct sl_span sl_fs_slurp_file(const char filename[const static 1]) {
+  struct sl_iterator file_iter = sl_file_iter_open(filename);
+  struct sl_span data = {0};
+  for (; !sl_file_iter_is_done(&file_iter); sl_file_iter_advance(&file_iter)) {
+    struct sl_span* buffer = sl_file_iter_get(&file_iter);
+    sl_span_extend(&data, buffer);
+  }
+  sl_file_iter_close(&file_iter);
+  return data;
+}
+
+#endif  // _STUFFLIB_FILESYSTEM_H_INCLUDED
---
+++ src/tests/test_stufflib_filesystem.c
@@ -0,0 +1,21 @@
+#include <assert.h>
+#include <stdlib.h>
+
+#include "stufflib_args.h"
+#include "stufflib_filesystem.h"
+#include "stufflib_macros.h"
+#include "stufflib_span.h"
+#include "stufflib_test_data.h"
+
+bool test_slurp_file(const bool verbose) {
+  for (size_t i = 0; i < SL_ARRAY_LEN(sl_test_data_file_paths); ++i) {
+    struct sl_span data = sl_fs_slurp_file(sl_test_data_file_paths[i]);
+    assert(data.owned);
+    assert(data.size == sl_test_data_file_sizes[i]);
+    assert(data.data != nullptr);
+    sl_span_delete(&data);
+  }
+  return true;
+}
+
+SL_TEST_MAIN(test_slurp_file)
