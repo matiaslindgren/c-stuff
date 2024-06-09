a5756e4a3778f51fb3c8ae3c25fe1e874e6398a0
Matias Lindgren <matias.lindgren@iki.fi>
2024-06-10 00:57:51 +0200

    fix blas

--- include/stufflib_blas.h
+++ include/stufflib_blas.h
@@ -1,6 +1,8 @@
 #ifndef SL_BLAS_H_INCLUDED
 #define SL_BLAS_H_INCLUDED
 
+#include <math.h>
+
 #ifdef __APPLE__
   #if __has_include(<Accelerate/Accelerate.h>)
     #define ACCELERATE_NEW_LAPACK
@@ -32,7 +34,7 @@ struct sl_blas_matrix {
   float* data;
 };
 
-struct sl_blas_matrix sl_blas_matrix_zeros(const int rows, const int cols) {
+struct sl_blas_matrix sl_blas_matrix_create(const int rows, const int cols) {
   if (rows < 0 || cols < 0) {
     SL_LOG_ERROR("cannot allocate a matrix with negative dimensions");
     return (struct sl_blas_matrix){0};
@@ -49,29 +51,79 @@ void sl_blas_matrix_destroy(struct sl_blas_matrix a[const static 1]) {
   *a = (struct sl_blas_matrix){0};
 }
 
-float* sl_blas_matrix_get(struct sl_blas_matrix a[const static 1],
-                          const int row,
-                          const int col) {
+static inline float* sl_blas_matrix_get(struct sl_blas_matrix a[const static 1],
+                                        const int row,
+                                        const int col) {
   return a->data + row * a->cols + col;
 }
 
-void sl_blas_matmul_f(const struct sl_blas_matrix a[const static 1],
-                      const struct sl_blas_matrix b[const static 1],
-                      struct sl_blas_matrix c[const static 1]) {
-  cblas_sgemm(CblasRowMajor,
-              CblasNoTrans,
-              CblasNoTrans,
-              a->rows,
-              b->cols,
-              b->cols,
-              1.0f,
-              a->data,
-              a->rows,
-              b->data,
-              b->rows,
-              1.0f,
-              c->data,
-              c->rows);
+void sl_blas_matrix_print(FILE stream[const static 1],
+                          struct sl_blas_matrix a[const static 1]) {
+  for (int row = 0; row < a->rows; ++row) {
+    for (int col = 0; col < a->cols; ++col) {
+      float value = *sl_blas_matrix_get(a, row, col);
+      if (fprintf(stream, "%.3f ", value) < 0) {
+        SL_LOG_ERROR("failed printing matrix value at (%d, %d): %.3g",
+                     row,
+                     col,
+                     value);
+        return;
+      }
+    }
+    if (fprintf(stream, "\n") < 0) {
+      SL_LOG_ERROR("failed printing newline after row %d", row);
+      return;
+    }
+  }
+}
+
+void sl_blas_matrix_multiply(const struct sl_blas_matrix a[const static 1],
+                             const struct sl_blas_matrix b[const static 1],
+                             struct sl_blas_matrix c[const static 1]) {
+  // c := a b
+  if (a->cols == b->rows && a->rows == c->rows && b->cols == c->cols) {
+    // column-major implicit transpose trickery from
+    // https://stackoverflow.com/a/56064726/5951112
+    // 2024-06-10
+    cblas_sgemm(CblasColMajor,
+                CblasNoTrans,
+                CblasNoTrans,
+                b->cols, /* m */
+                a->rows, /* n */
+                a->cols, /* k */
+                1,       /* alpha */
+                b->data, /* a */
+                b->cols, /* lda */
+                a->data, /* b */
+                a->cols, /* ldb */
+                0,       /* beta */
+                c->data, /* c */
+                c->cols  /* ldc */
+    );
+  } else {
+    SL_LOG_ERROR("mismatching matrix dimensions, will not multiply");
+  }
+}
+
+double sl_blas_matrix_trace(struct sl_blas_matrix a[const static 1]) {
+  double tr = 0;
+  for (int i = 0; i < SL_MIN(a->rows, a->cols); ++i) {
+    tr += *sl_blas_matrix_get(a, i, i);
+  }
+  return tr;
+}
+
+double sl_blas_matrix_frobenius_norm(struct sl_blas_matrix a[const static 1]) {
+  // https://en.wikipedia.org/wiki/Matrix_norm#Frobenius_norm
+  // 2024-06-09
+  double norm = 0;
+  for (int row = 0; row < a->rows; ++row) {
+    for (int col = 0; col < a->cols; ++col) {
+      const double value = *sl_blas_matrix_get(a, row, col);
+      norm += pow(fabs(value), 2);
+    }
+  }
+  return sqrt(norm);
 }
 
 #endif  // SL_BLAS_H_INCLUDED
--- src/tests/test_stufflib_blas.c
+++ src/tests/test_stufflib_blas.c
@@ -9,27 +9,26 @@
 
 bool fequal(const double a, const double b) { return fabs(a - b) < 1e-6; }
 
-bool check_matrix_equal(const struct sl_blas_matrix a[const static 1],
-                        const struct sl_blas_matrix b[const static 1]) {
-  if (a->rows != b->rows) {
-    return false;
-  }
-  if (a->cols != b->cols) {
-    return false;
-  }
+bool check_matrix_equal(struct sl_blas_matrix a[const static 1],
+                        struct sl_blas_matrix b[const static 1]) {
+  bool ok = (a->rows == b->rows && a->cols == b->cols);
   const int rows = a->rows;
   const int cols = a->cols;
-  for (int row = 0; row < rows; ++row) {
-    for (int col = 0; col < cols; ++col) {
+  for (int row = 0; ok && row < rows; ++row) {
+    for (int col = 0; ok && col < cols; ++col) {
       const float a_val = a->data[row * cols + col];
       const float b_val = b->data[row * cols + col];
-      if (!fequal(a_val, b_val)) {
-        fprintf(stderr, "(row %d, col %d) %g != %g\n", row, col, a_val, b_val);
-        return false;
-      }
+      ok = fequal(a_val, b_val);
     }
   }
-  return true;
+  if (!ok) {
+    fprintf(stderr, "!expected matrix a and b to be equal\n");
+    fprintf(stderr, "a:\n");
+    sl_blas_matrix_print(stderr, a);
+    fprintf(stderr, "b:\n");
+    sl_blas_matrix_print(stderr, b);
+  }
+  return ok;
 }
 
 bool test_matrix_get(const bool) {
@@ -46,41 +45,126 @@ bool test_matrix_get(const bool) {
   return true;
 }
 
-bool test_matmul_square(const bool) {
-  const struct sl_blas_matrix a = {
+bool test_matrix_create(const bool) {
+  {
+    struct sl_blas_matrix a = sl_blas_matrix_create(1, 1);
+    sl_blas_matrix_destroy(&a);
+  }
+  {
+    struct sl_blas_matrix a = sl_blas_matrix_create(1, 10);
+    sl_blas_matrix_destroy(&a);
+  }
+  {
+    struct sl_blas_matrix a = sl_blas_matrix_create(10, 1);
+    sl_blas_matrix_destroy(&a);
+  }
+  {
+    struct sl_blas_matrix a = sl_blas_matrix_create(1024, 4096);
+    sl_blas_matrix_destroy(&a);
+  }
+  {
+    struct sl_blas_matrix a = sl_blas_matrix_create(65536, 256);
+    sl_blas_matrix_destroy(&a);
+  }
+  return true;
+}
+
+bool test_matrix_trace(const bool) {
+  struct sl_blas_matrix a = {
       .rows = 3,
       .cols = 3,
       .data = (float[]){0, 1, 2, 3, 4, 5, 6, 7, 8}
   };
-  struct sl_blas_matrix result = sl_blas_matrix_zeros(a.rows, a.cols);
+  double tr = sl_blas_matrix_trace(&a);
+  double expected = 0 + 4 + 8;
+  if (fequal(tr, expected)) {
+    return true;
+  }
+  fprintf(stderr, "trace should be %g, not %g\n", expected, tr);
+  return false;
+}
 
-  sl_blas_matmul_f(&a, &a, &result);
-
-  const struct sl_blas_matrix expected = {
+bool test_matrix_multiply_square(const bool) {
+  struct sl_blas_matrix a = {
+      .rows = 3,
+      .cols = 3,
+      .data = (float[]){0, 1, 2, 3, 4, 5, 6, 7, 8}
+  };
+  struct sl_blas_matrix result = {
+      .rows = 3,
+      .cols = 3,
+      .data = (float[3 * 3]){0},
+  };
+  sl_blas_matrix_multiply(&a, &a, &result);
+  struct sl_blas_matrix expected = {
       .rows = a.rows,
       .cols = a.cols,
       .data = (float[]){15, 18, 21, 42, 54, 66, 69, 90, 111}
   };
-  bool ok = check_matrix_equal(&result, &expected);
-  sl_blas_matrix_destroy(&result);
-  return ok;
+  return check_matrix_equal(&result, &expected);
 }
 
-bool test_matmul_zeros(const bool) {
-  const struct sl_blas_matrix a = {
+bool test_matrix_multiply_zeros(const bool) {
+  struct sl_blas_matrix a = {
       .rows = 3,
       .cols = 3,
-      .data = (float[]){0, 1, 2, 3, 4, 5, 6, 7, 8}
+      .data = (float[]){0, 1, 2, 3, 4, 5, 6, 7, 8},
   };
-  struct sl_blas_matrix b = sl_blas_matrix_zeros(a.rows, a.cols);
+  struct sl_blas_matrix b = {
+      .rows = 3,
+      .cols = 3,
+      .data = (float[3 * 3]){0},
+  };
   struct sl_blas_matrix result = b;
-
-  sl_blas_matmul_f(&a, &b, &result);
-
-  const struct sl_blas_matrix expected = b;
-  bool ok = check_matrix_equal(&result, &expected);
-  sl_blas_matrix_destroy(&b);
-  return ok;
+  struct sl_blas_matrix expected = b;
+  sl_blas_matrix_multiply(&a, &b, &result);
+  return check_matrix_equal(&result, &expected);
 }
 
-SL_TEST_MAIN(test_matrix_get, test_matmul_square, test_matmul_zeros)
+bool test_matrix_multiply(const bool) {
+  struct sl_blas_matrix a = {
+      .rows = 4,
+      .cols = 3,
+      .data = (float[]){0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}
+  };
+  struct sl_blas_matrix b = {
+      .rows = 3,
+      .cols = 2,
+      .data = (float[]){-3, -1, 1, 3, 5, 7}
+  };
+  struct sl_blas_matrix result = {
+      .rows = a.rows,
+      .cols = b.cols,
+      .data = (float[4 * 2]){0},
+  };
+  sl_blas_matrix_multiply(&a, &b, &result);
+  struct sl_blas_matrix expected = {
+      .rows = a.rows,
+      .cols = b.cols,
+      .data = (float[]){11, 17, 20, 44, 29, 71, 38, 98}
+  };
+  return check_matrix_equal(&result, &expected);
+}
+
+bool test_matrix_frobenius_norm(const bool) {
+  struct sl_blas_matrix a = {
+      .rows = 4,
+      .cols = 3,
+      .data = (float[]){0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}
+  };
+  double norm = sl_blas_matrix_frobenius_norm(&a);
+  double expected = 22.494443758403985;
+  if (fequal(norm, expected)) {
+    return true;
+  }
+  fprintf(stderr, "frobenius norm should be %g, not %g\n", expected, norm);
+  return false;
+}
+
+SL_TEST_MAIN(test_matrix_get,
+             test_matrix_create,
+             test_matrix_trace,
+             test_matrix_multiply_square,
+             test_matrix_multiply_zeros,
+             test_matrix_multiply,
+             test_matrix_frobenius_norm)
