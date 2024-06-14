f872f315ae2959ff91b3d19aa28ac588f28b045a
Matias Lindgren <matias.lindgren@iki.fi>
2024-06-14 23:15:50 +0200

    implement randint and shuffle

--- include/stufflib_rand.h
+++ include/stufflib_rand.h
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 #include <time.h>
 
+#include "stufflib_misc.h"
+
 void sl_rand_fill_double(const size_t n, double dst[n], double scale) {
   for (size_t i = 0; i < n; ++i) {
     double r = rand() - RAND_MAX / 2;
@@ -19,6 +21,27 @@ void sl_rand_set_zero_double(const size_t n,
     if ((1.0 * rand() / RAND_MAX) < probability) {
       dst[i] = 0;
     }
+  }
+}
+
+int sl_rand_int(const int a, const int b) {
+  // return random integer i such that a <= i < b
+  if (a >= b) {
+    return a;
+  }
+  return a + (rand() % (b - a));
+}
+
+void sl_rand_shuffle(void* data, const int count, const int size) {
+  // https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+  // 2024-06-14
+  if (size <= 0 || count < 2) {
+    return;
+  }
+  unsigned char* v = data;
+  for (int i = 0; i < count - 1; ++i) {
+    const int j = sl_rand_int(i, count);
+    sl_misc_swap(v + i * size, v + j * size, (size_t)size);
   }
 }
 
--- src/tests/test_stufflib_rand.c
+++ src/tests/test_stufflib_rand.c
@@ -47,4 +47,71 @@ bool test_rand_set_zero(const bool) {
   return true;
 }
 
-SL_TEST_MAIN(test_rand_fill, test_rand_set_zero)
+bool test_randint(const bool) {
+  assert(sl_rand_int(0, 0) == 0);
+  assert(sl_rand_int(1, 0) == 1);
+  assert(sl_rand_int(0, 1) == 0);
+  for (int a = 1; a < 100; ++a) {
+    for (int b = a + 1; b < 100; ++b) {
+      int freq[100] = {0};
+      for (int i = 0; i < 2000; ++i) {
+        int x = sl_rand_int(a, b);
+        assert(a <= x && x < b);
+        freq[x] += 1;
+      }
+      for (int x = a; x < b; ++x) {
+        assert(0 < freq[x] && freq[x] <= 2000);
+      }
+    }
+  }
+  return true;
+}
+
+bool test_rand_shuffle(const bool verbose) {
+  {
+    unsigned char v[] = {0};
+    sl_rand_shuffle(v, 1, 1);
+    assert(v[0] == 0);
+  }
+  {
+    unsigned char v[16] = {0};
+    int freq_unshuffled[SL_ARRAY_LEN(v)] = {0};
+    const size_t n = SL_ARRAY_LEN(v);
+    for (unsigned char i = 0; i < n; ++i) {
+      v[i] = i;
+    }
+
+    for (int iter = 0; iter < 100; ++iter) {
+      unsigned char buf[16] = {0};
+      memcpy(buf, v, n);
+      sl_rand_shuffle(buf, n, 1);
+      if (verbose) {
+        printf("shuffle %02d: ", iter);
+        for (size_t i = 0; i < n; ++i) {
+          printf("%d ", buf[i]);
+        }
+        printf("\n");
+      }
+      for (size_t i = 0; i < n; ++i) {
+        freq_unshuffled[i] += buf[i] == v[i];
+      }
+    }
+
+    if (verbose) {
+      printf("seen counts\n");
+      for (size_t i = 0; i < n; ++i) {
+        printf("  %d: %d\n", v[i], freq_unshuffled[i]);
+      }
+      printf("\n");
+    }
+    for (size_t i = 0; i < n; ++i) {
+      assert(freq_unshuffled[i] < 50);
+    }
+  }
+  return true;
+}
+
+SL_TEST_MAIN(test_rand_fill,
+             test_rand_set_zero,
+             test_randint,
+             test_rand_shuffle)
