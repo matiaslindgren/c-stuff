3e49927a514e00a029c6ac95603989fbde4084bc
Matias Lindgren <matias.lindgren@iki.fi>
2025-04-20 19:21:10 -0400

    add tcp server

--- tests/integration/test_server_tool.bash
+++ tests/integration/test_server_tool.bash
@@ -1,2 +1,38 @@
 #!/usr/bin/env bash
-set -ue
+set -xue
+
+self_dir=$(dirname "$0")
+source ${self_dir}/common.bash $@
+
+server_tool="$1"
+nohup $server_tool > server.out 2>&1 &
+server_pid=$!
+trap "kill $server_pid" EXIT TERM
+
+while [ ! -s server.out ]; do
+  sleep 1
+done
+trap "rm server.out" EXIT TERM
+
+function check_no_server_errors {
+  if [ "$(jq .level server.out | grep error --count)" -gt 0 ]; then
+    cat server.out
+    exit 1
+  fi
+}
+
+check_no_server_errors
+
+python3 -c '\
+import socket
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect(("127.0.0.1", 8080))
+    data = s.recv(1024)
+    print(data.decode("utf-8").strip())
+' > client.out 2>&1
+
+check_no_server_errors
+if ! grep --silent hello client.out; then
+  cat client.out
+  exit 1
+fi
--- tools/server.c
+++ tools/server.c
@@ -1,132 +1,100 @@
-#if 0
-  #include <arpa/inet.h>
-  #include <errno.h>
-  #include <netdb.h>
-  #include <netinet/in.h>
-  #include <signal.h>
-  #include <stdio.h>
-  #include <stdlib.h>
-  #include <string.h>
-  #include <sys/socket.h>
-  #include <sys/types.h>
-  #include <sys/wait.h>
-  #include <unistd.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-  #define PORT "3490"  // the port users will be connecting to
+#include "stufflib/macros/macros.h"
+#include "stufflib/misc/misc.h"
 
-  #define BACKLOG 10  // how many pending connections queue will hold
+#define SL_SOCKET_BACKLOG_LEN 1024
 
-  #pragma GCC diagnostic push
-  #pragma GCC diagnostic ignored "-Wcast-align"
+static bool sl_server_running = false;
 
-void sigchld_handler(int s) {
-  (void)s;
-  // waitpid() might overwrite errno, so we save and restore it:
-  int saved_errno = errno;
-  for (; waitpid(-1, nullptr, WNOHANG) > 0;) {
-  }
-  errno = saved_errno;
-}
-
-// get sockaddr, IPv4 or IPv6:
-void *get_in_addr(struct sockaddr sa[const static 1]) {
-  if (sa->sa_family == AF_INET) {
-    return &(((struct sockaddr_in *)sa)->sin_addr);
-  }
-  return &(((struct sockaddr_in6 *)sa)->sin6_addr);
+void stop_server_on_sigterm(int) {
+  SL_LOG_INFO("received SIGTERM, shutting down");
+  sl_server_running = false;
 }
 
 int main(void) {
-  int sockfd = 0;
-  int new_fd = 0;  // listen on sock_fd, new connection on new_fd
-  struct addrinfo hints = {};
-  struct addrinfo *servinfo = nullptr;
-  struct addrinfo *p = nullptr;
-  struct sockaddr_storage their_addr = {};  // connector's address information
-  socklen_t sin_size = 0;
-  struct sigaction sa = {};
-  int yes = 1;
-  char s[INET6_ADDRSTRLEN] = {};
-  int rv = 0;
-
-  hints.ai_family = AF_UNSPEC;
-  hints.ai_socktype = SOCK_STREAM;
-  hints.ai_flags = AI_PASSIVE;  // use my IP
-
-  if ((rv = getaddrinfo(nullptr, PORT, &hints, &servinfo)) != 0) {
-    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+  int fd_listen = 0;
+  if (0 > (fd_listen = socket(AF_INET, SOCK_STREAM, 0))) {
+    SL_LOG_ERROR("failed creating socket");
+    perror("error");  // TODO into SL_LOG
     return 1;
   }
 
-  // loop through all the results and bind to the first we can
-  for (p = servinfo; p != nullptr; p = p->ai_next) {
-    if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
-      perror("server: socket");
-      continue;
-    }
+  struct sockaddr_in serveraddr = {
+      .sin_family = AF_INET,
+      .sin_addr.s_addr = htonl(INADDR_ANY),
+      .sin_port = htons(8080),
+  };
 
-    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
-      perror("setsockopt");
-      exit(1);
-    }
-
-    if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
-      close(sockfd);
-      perror("server: bind");
-      continue;
-    }
-
-    break;
+  if (0 > bind(fd_listen, (struct sockaddr *)&serveraddr, sizeof(serveraddr))) {
+    SL_LOG_ERROR("failed binding address to socket %d", fd_listen);
+    perror("error");  // TODO into SL_LOG
+    return 1;
   }
 
-  freeaddrinfo(servinfo);  // all done with this structure
-
-  if (p == nullptr) {
-    fprintf(stderr, "server: failed to bind\n");
-    exit(1);
+  if (0 > listen(fd_listen, SL_SOCKET_BACKLOG_LEN)) {
+    SL_LOG_ERROR("failed listening to socket %d", fd_listen);
+    perror("error");  // TODO into SL_LOG
+    return 1;
   }
 
-  if (listen(sockfd, BACKLOG) == -1) {
-    perror("listen");
-    exit(1);
-  }
-
-  sa.sa_handler = sigchld_handler;  // reap all dead processes
-  sigemptyset(&sa.sa_mask);
-  sa.sa_flags = SA_RESTART;
-  if (sigaction(SIGCHLD, &sa, nullptr) == -1) {
-    perror("sigaction");
-    exit(1);
-  }
-
-  printf("server: waiting for connections...\n");
-
-  while (1) {  // main accept() loop
-    sin_size = sizeof their_addr;
-    new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
-    if (new_fd == -1) {
-      perror("accept");
-      continue;
+  {
+    struct sigaction sa = {
+        .sa_flags = 0,
+        .sa_handler = stop_server_on_sigterm,
+    };
+    sigemptyset(&sa.sa_mask);
+    if (0 > sigaction(SIGTERM, &sa, nullptr)) {
+      SL_LOG_ERROR("failed installing sigterm handler");
+      perror("error");  // TODO into SL_LOG
+      return 1;
     }
+  }
 
-    inet_ntop(their_addr.ss_family,
-              get_in_addr((struct sockaddr *)&their_addr),
-              s,
-              sizeof s);
-    printf("server: got connection from %s\n", s);
-
-    if (!fork()) {    // this is the child process
-      close(sockfd);  // child doesn't need the listener
-      if (send(new_fd, "Hello, world!", 13, 0) == -1) perror("send");
-      close(new_fd);
-      break;
+  sl_server_running = true;
+  SL_LOG_INFO("start serving");
+  for (char msg_buffer[4096] = {}; sl_server_running;) {
+    bool ok = true;
+    int fd_conn = 0;
+    if (0 >
+        (fd_conn = accept(fd_listen, (struct sockaddr *)nullptr, nullptr))) {
+      SL_LOG_ERROR("failed accepting incoming connection");
+      perror("error");  // TODO into SL_LOG
+      ok = false;
+      goto end;
     }
-    close(new_fd);  // parent doesn't need this
+    SL_LOG_INFO("handling TCP request");
+    if (0 > snprintf(msg_buffer, SL_ARRAY_LEN(msg_buffer), "hello\r\n")) {
+      SL_LOG_ERROR("failed writing message buffer");
+      perror("error");  // TODO into SL_LOG
+      ok = false;
+      goto end;
+    }
+    if (0 > send(fd_conn, msg_buffer, strlen(msg_buffer), 0)) {
+      SL_LOG_ERROR("failed sending message");
+      perror("error");  // TODO into SL_LOG
+      ok = false;
+      goto end;
+    }
+  end:
+    if (fd_conn >= 0 && 0 > close(fd_conn)) {
+      SL_LOG_ERROR("failed closing %d", fd_conn);
+      perror("error");  // TODO into SL_LOG
+      ok = false;
+    }
+    if (!ok) {
+      return 1;
+    }
   }
 
   return 0;
 }
-
-  #pragma GCC diagnostic pop
-#endif
-int main() { return 0; }
