8396429be85a7fcbf7e0a26f067141b5134f2228
Matias Lindgren <matias.lindgren@iki.fi>
2025-04-21 22:26:58 -0400

    parse host:port from args, improve error handling

--- tools/server.c
+++ tools/server.c
@@ -9,6 +9,7 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+#include "stufflib/args/args.h"
 #include "stufflib/macros/macros.h"
 #include "stufflib/misc/misc.h"
 
@@ -21,30 +22,76 @@ void stop_server_on_sigterm(int) {
   sl_server_running = false;
 }
 
-int main(void) {
-  int fd_listen = 0;
+void print_usage(const struct sl_args args[const static 1]) {
+  fprintf(stderr, "usage: %s [-h | --help] host port\n", args->argv[0]);
+}
+
+int main(int argc, char *const argv[argc + 1]) {
+  struct sockaddr_in serveraddr = {
+      .sin_family = AF_INET,
+  };
+
+  bool ok = true;
+  {
+    struct sl_args args = {.argc = argc, .argv = argv};
+    if (sl_args_contains_help_flag(&args)) {
+      print_usage(&args);
+      return 2;
+    }
+    if (sl_args_count_positional(&args) != 2) {
+      ok = false;
+    } else {
+      switch (inet_pton(AF_INET, argv[1], &(serveraddr.sin_addr))) {
+        case -1: {
+          SL_LOG_ERROR("error during parsing of host IPv4 address");
+          ok = false;
+        } break;
+        case 0: {
+          SL_LOG_ERROR("host address is not IPv4");
+          ok = false;
+        } break;
+        case 1: {
+          SL_LOG_TRACE("input %s is a valid IPv4 address", argv[1]);
+          ok = true;
+        } break;
+      }
+      if (ok) {
+        const unsigned long port_arg = strtoul(argv[2], nullptr, 10);
+        if (0 < port_arg && port_arg < (1UL << 16)) {
+          serveraddr.sin_port = htons((int)port_arg);
+        } else {
+          SL_LOG_ERROR("input port must be in [1, 65535]");
+          ok = false;
+        }
+      }
+    }
+    if (!ok) {
+      print_usage(&args);
+      return 1;
+    }
+  }
+
+  int fd_listen = -1;
+
   if (0 > (fd_listen = socket(AF_INET, SOCK_STREAM, 0))) {
     SL_LOG_ERROR("failed creating socket");
     perror("error");  // TODO into SL_LOG
-    return 1;
+    ok = false;
+    goto end;
   }
-
-  struct sockaddr_in serveraddr = {
-      .sin_family = AF_INET,
-      .sin_addr.s_addr = htonl(INADDR_ANY),
-      .sin_port = htons(8080),
-  };
 
   if (0 > bind(fd_listen, (struct sockaddr *)&serveraddr, sizeof(serveraddr))) {
     SL_LOG_ERROR("failed binding address to socket %d", fd_listen);
     perror("error");  // TODO into SL_LOG
-    return 1;
+    ok = false;
+    goto end;
   }
 
   if (0 > listen(fd_listen, SL_SOCKET_BACKLOG_LEN)) {
     SL_LOG_ERROR("failed listening to socket %d", fd_listen);
     perror("error");  // TODO into SL_LOG
-    return 1;
+    ok = false;
+    goto end;
   }
 
   {
@@ -56,45 +103,53 @@ int main(void) {
     if (0 > sigaction(SIGTERM, &sa, nullptr)) {
       SL_LOG_ERROR("failed installing sigterm handler");
       perror("error");  // TODO into SL_LOG
-      return 1;
+      ok = false;
+      goto end;
     }
   }
 
   sl_server_running = true;
-  SL_LOG_INFO("start serving");
+  SL_LOG_INFO("listening at %s:%s", argv[1], argv[2]);
+
   for (char msg_buffer[4096] = {}; sl_server_running;) {
-    bool ok = true;
-    int fd_conn = 0;
+    int fd_conn = -1;
     if (0 >
         (fd_conn = accept(fd_listen, (struct sockaddr *)nullptr, nullptr))) {
       SL_LOG_ERROR("failed accepting incoming connection");
       perror("error");  // TODO into SL_LOG
       ok = false;
-      goto end;
+      goto end_loop;
     }
     SL_LOG_INFO("handling TCP request");
     if (0 > snprintf(msg_buffer, SL_ARRAY_LEN(msg_buffer), "hello\r\n")) {
       SL_LOG_ERROR("failed writing message buffer");
       perror("error");  // TODO into SL_LOG
       ok = false;
-      goto end;
+      goto end_loop;
     }
     if (0 > send(fd_conn, msg_buffer, strlen(msg_buffer), 0)) {
       SL_LOG_ERROR("failed sending message");
       perror("error");  // TODO into SL_LOG
       ok = false;
-      goto end;
+      goto end_loop;
     }
-  end:
-    if (fd_conn >= 0 && 0 > close(fd_conn)) {
+  end_loop:
+    if (fd_conn != -1 && 0 > close(fd_conn)) {
       SL_LOG_ERROR("failed closing %d", fd_conn);
       perror("error");  // TODO into SL_LOG
       ok = false;
     }
     if (!ok) {
-      return 1;
+      sl_server_running = false;
     }
   }
 
-  return 0;
+end:
+  if (fd_listen != -1 && 0 > close(fd_listen)) {
+    SL_LOG_ERROR("failed closing %d", fd_listen);
+    perror("error");  // TODO into SL_LOG
+    ok = false;
+  }
+
+  return ok ? 0 : 1;
 }
