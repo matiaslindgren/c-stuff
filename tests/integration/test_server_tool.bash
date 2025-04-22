b731ca1ad80adddaf8a689d162fd423391aafa3e
Matias Lindgren <matias.lindgren@iki.fi>
2025-04-21 22:57:37 -0400

    echo tcp message back to client

--- tests/integration/test_server_tool.bash
+++ tests/integration/test_server_tool.bash
@@ -5,6 +5,7 @@ self_dir=$(dirname "$0")
 source ${self_dir}/common.bash $@
 
 listen_port=8080
+send_message="hello TCP server"
 
 # wait until TCP sockets from previous runs end their TIME_WAIT state
 while $(netstat -n | grep -Eq '^tcp.*127\.0\.0\.1[:.]'${listen_port}); do
@@ -40,12 +41,13 @@ python3 -c "\
 import socket
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect(('127.0.0.1', $listen_port))
+    s.sendall(b'$send_message')
     data = s.recv(1024)
     print(data.decode('utf-8').strip())
 " > client.out 2>&1
 
 check_no_server_errors
-if ! grep --silent hello client.out; then
+if ! grep --silent "$send_message" client.out; then
   cat client.out
   exit 1
 fi
--- tools/server.c
+++ tools/server.c
@@ -1,4 +1,5 @@
 #include <arpa/inet.h>
+#include <errno.h>
 #include <netdb.h>
 #include <netinet/in.h>
 #include <signal.h>
@@ -38,6 +39,7 @@ int main(int argc, char *const argv[argc + 1]) {
       print_usage(&args);
       return 2;
     }
+
     if (sl_args_count_positional(&args) != 2) {
       ok = false;
     } else {
@@ -55,6 +57,7 @@ int main(int argc, char *const argv[argc + 1]) {
           ok = true;
         } break;
       }
+
       if (ok) {
         const unsigned long port_arg = strtoul(argv[2], nullptr, 10);
         if (0 < port_arg && port_arg < (1UL << 16)) {
@@ -65,6 +68,7 @@ int main(int argc, char *const argv[argc + 1]) {
         }
       }
     }
+
     if (!ok) {
       print_usage(&args);
       return 1;
@@ -115,30 +119,64 @@ int main(int argc, char *const argv[argc + 1]) {
     int fd_conn = -1;
     if (0 >
         (fd_conn = accept(fd_listen, (struct sockaddr *)nullptr, nullptr))) {
-      SL_LOG_ERROR("failed accepting incoming connection");
-      perror("error");  // TODO into SL_LOG
-      ok = false;
-      goto end_loop;
+      if (errno == EINTR) {
+        SL_LOG_INFO("received interrupt signal, will not accept connections");
+        goto end_loop;
+      } else {
+        SL_LOG_ERROR("failed accepting incoming connection");
+        perror("error");  // TODO into SL_LOG
+        ok = false;
+        goto end_loop;
+      }
     }
-    SL_LOG_INFO("handling TCP request");
+
+    SL_LOG_INFO("handling TCP request on fd %d", fd_conn);
+
+    {
+      ssize_t msg_len = 0;
+      if (0 >
+          (msg_len = recv(fd_conn, msg_buffer, SL_ARRAY_LEN(msg_buffer), 0))) {
+        SL_LOG_ERROR("failed reading message on %d", fd_conn);
+        perror("error");  // TODO into SL_LOG
+        ok = false;
+        goto end_loop;
+      }
+
+      if (msg_len >= (ssize_t)SL_ARRAY_LEN(msg_buffer)) {
+        SL_LOG_ERROR(
+            "message sent by client is too large (%zd), truncating to %zu",
+            msg_len,
+            SL_ARRAY_LEN(msg_buffer));
+        msg_len = (ssize_t)(SL_ARRAY_LEN(msg_buffer) - 1);
+      }
+      msg_buffer[msg_len] = 0;
+    }
+
+    SL_LOG_TRACE("received message '%s' on %d", msg_buffer);
+
+#if 0
     if (0 > snprintf(msg_buffer, SL_ARRAY_LEN(msg_buffer), "hello\r\n")) {
       SL_LOG_ERROR("failed writing message buffer");
       perror("error");  // TODO into SL_LOG
       ok = false;
       goto end_loop;
     }
+#endif
+
     if (0 > send(fd_conn, msg_buffer, strlen(msg_buffer), 0)) {
-      SL_LOG_ERROR("failed sending message");
+      SL_LOG_ERROR("failed sending message on %d", fd_conn);
       perror("error");  // TODO into SL_LOG
       ok = false;
       goto end_loop;
     }
+
   end_loop:
     if (fd_conn != -1 && 0 > close(fd_conn)) {
       SL_LOG_ERROR("failed closing %d", fd_conn);
       perror("error");  // TODO into SL_LOG
       ok = false;
     }
+
     if (!ok) {
       sl_server_running = false;
     }
