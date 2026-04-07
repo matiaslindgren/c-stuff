e26931132faa68d9893efec17d767326d0840497
Matias Lindgren <matias.lindgren@iki.fi>
2026-04-06 22:15:49 -0400

    implement error stack unwind

--- stufflib/context/context.c
+++ stufflib/context/context.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
 #include <stufflib/context/context.h>
+#include <stufflib/error/error.h>
+#include <stufflib/macros/macros.h>
+
+void sl_context_unwind_errors(struct sl_context ctx[static 1], FILE stream[static 1]) {
+  for (struct sl_error_msg e = {0}; sl_error_pop(&ctx->errors, &e);) {
+    if (fprintf(
+            stream,
+            ("{\"level\":\"error\""
+             ",\"file\":\"%s\""
+             ",\"line\":%d"
+             ",\"msg\":\"%s\"}\n"),
+            e.file,
+            e.line,
+            e.msg
+        )
+        < 0) {
+      abort();
+    }
+  }
+}
--- tests/test_stufflib_context.c
+++ tests/test_stufflib_context.c
@@ -1,4 +1,5 @@
 #include <assert.h>
+#include <stdio.h>
 #include <string.h>
 #include <stufflib/args/args.h>
 #include <stufflib/context/context.h>
@@ -98,10 +99,84 @@ static bool test_sl_error_macro_msg_truncation(struct sl_context ctx[static 1], 
 
   SL_ERROR(&c, "%s", long_str);
 
+  assert(strlen(long_str) > SL_ERROR_MSG_LEN - 1);
+
   const struct sl_error_msg* top = sl_error_peek(&c.errors);
   assert(top != nullptr);
   assert(strlen(top->msg) == SL_ERROR_MSG_LEN - 1);
+  assert(memcmp(top->msg, long_str, SL_ERROR_MSG_LEN - 1) == 0);
 
+  return true;
+}
+
+static bool test_unwind_empties_stack(struct sl_context ctx[static 1], const bool) {
+  (void)ctx;
+  struct sl_context c = {0};
+
+  SL_ERROR(&c, "first");
+  SL_ERROR(&c, "second");
+  SL_ERROR(&c, "third");
+
+  char* buf   = nullptr;
+  size_t size = 0;
+  FILE* mem   = open_memstream(&buf, &size);
+  assert(mem);
+
+  sl_context_unwind_errors(&c, mem);
+  fclose(mem);
+
+  assert(!sl_error_occurred(&c.errors));
+  assert(sl_error_depth(&c.errors) == 0);
+
+  // LIFO: third printed first, then second, then first
+  assert(strstr(buf, "third") != nullptr);
+  assert(strstr(buf, "second") != nullptr);
+  assert(strstr(buf, "first") != nullptr);
+  assert(strstr(buf, "third") < strstr(buf, "second"));
+  assert(strstr(buf, "second") < strstr(buf, "first"));
+
+  free(buf);
+  return true;
+}
+
+static bool test_unwind_output_format(struct sl_context ctx[static 1], const bool) {
+  (void)ctx;
+  struct sl_context c = {0};
+
+  SL_ERROR(&c, "boom");
+
+  char* buf   = nullptr;
+  size_t size = 0;
+  FILE* mem   = open_memstream(&buf, &size);
+  assert(mem);
+
+  sl_context_unwind_errors(&c, mem);
+  fclose(mem);
+
+  assert(strstr(buf, "\"level\":\"error\"") != nullptr);
+  assert(strstr(buf, "\"file\":\"") != nullptr);
+  assert(strstr(buf, "\"line\":") != nullptr);
+  assert(strstr(buf, "\"msg\":\"boom\"}") != nullptr);
+
+  free(buf);
+  return true;
+}
+
+static bool test_unwind_empty_stack_is_noop(struct sl_context ctx[static 1], const bool) {
+  (void)ctx;
+  struct sl_context c = {0};
+
+  char* buf   = nullptr;
+  size_t size = 0;
+  FILE* mem   = open_memstream(&buf, &size);
+  assert(mem);
+
+  sl_context_unwind_errors(&c, mem);
+  fclose(mem);
+
+  assert(size == 0);
+
+  free(buf);
   return true;
 }
 
@@ -112,5 +187,8 @@ SL_TEST_MAIN(
     test_sl_error_macro_captures_file,
     test_sl_error_macro_captures_line,
     test_sl_error_macro_accumulates,
-    test_sl_error_macro_msg_truncation
+    test_sl_error_macro_msg_truncation,
+    test_unwind_empties_stack,
+    test_unwind_output_format,
+    test_unwind_empty_stack_is_noop
 )
